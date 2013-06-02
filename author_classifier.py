b5619ea43ce902b9a021b2d029593ff4b532093f
Tuur Leeuwenberg <t.leeuwenberg@gmail.com>
2013-06-02 14:15:58 +0200

    Opschoning1.0001

--- author_classifier.py
+++ author_classifier.py
@@ -2,10 +2,13 @@ from getcorpus import corpus
 from classifier import classify
 from nltk.corpus import gutenberg
 from help_functions import *
-wrdsC = gutenberg.words()
-wrds = [w.lower() for w in wrdsC]
 
-def test():
+
+
+def test(): 
+    """ Een test classification a.d.h.v. het gutenberg corpus. (Is het een korte of lange text? >100 / < 100wrd) """
+    wrdsC = gutenberg.words()
+    wrds = [w.lower() for w in wrdsC]
     trainingsteksten_lang = [(text,"lang") for text in [wrds[:x] for x in range(150,170)]] 
     trainingsteksten_kort = [(text,"kort") for text in [wrds[:x] for x in range(60,70)]] 
     trainingsteksten = trainingsteksten_lang + trainingsteksten_kort
@@ -13,10 +16,10 @@ def test():
     
 # lijst gewenste auteurs
 authors=["AaronPressman","AlanCrosby","AlexanderSmith","BenjaminKangLim","BernardHickey"]
-
-for a in authors:
-    print a + ":" 
-    print numberoftexts_cat(a,corpus())
     
+    
+test()
+
 print "\nAverage length of sentences per author:"
 print SentenceLengths(corpus())
+
