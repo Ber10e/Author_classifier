68e58d71af59bb18e2583b73668315b9ed48c9b7
Tuur Leeuwenberg <t.leeuwenberg@gmail.com>
2013-06-02 23:50:55 +0200

    Laatste versie die klopt met de presentatie.

--- author_classifier.py
+++ author_classifier.py
@@ -2,33 +2,45 @@ from getcorpus import corpus
 from classifier import classify
 from help_functions import *
 from time import time
+import pickle
+
 
 authors=["AaronPressman","AlanCrosby","AlexanderSmith","BenjaminKangLim","BernardHickey"]
-features = ["f1","f2","f3","f4","f5","f6","f7","f8","f9","f10","f11","f12","f13"]  
+features = ["f1","f2","f3","f4","f5","f6","f7","f8","f9","f10","f11","f12","f13","f14","f15"]  
 corpus=corpus()
 
 def test():
     start = time()
     #print "length testdata:" + str(len(testdata))
     #print "length traindata:" +str(len(traindata))
-    correct = 0
+    correct = {}
+    for a in authors:
+        correct[a]=0
     incorrect = 0
     runs = 0
-    for i in range(0,5):
-        data = split_train_test_data(authors, corpus,30)
+    
+    for i in range(0,8):
+        data = split_train_test_data(authors, corpus,45)
         testdata = data["test"]
         traindata = data["train"]
+        
         for j in range(0,len(testdata)):
             text = testdata[j]
             if (classify(text[0],traindata, features,authors)==text[1]):
-                correct +=1
+                correct[text[1]] +=1
                 runs +=1
+                print runs
             else:
                 incorrect +=1
                 runs +=1
-                
+                print runs
+    
+    totalcorrect = 0
+    for a in authors:
+        print a+str(correct[a])
+        totalcorrect += correct[a]
     print "runs:" + str(runs)
-    print "percentage correct:" + str(float(correct)/runs)
+    print "percentage correct:" + str(float(totalcorrect)/runs)
     print "runtime: " + str(time()-start) + " seconds"
     
     
@@ -38,13 +50,21 @@ test()
 
 #print "\nAverage length of sentences per author:"
 #print SentenceLengths(corpus())
-
-
 """
+def save_to_file(list,file):
+    f = open(file,'w')
+    for (x,y) in freqs:
+        f.write(x +"\t\t" +y + "\n")
+    f.close()
+freqs = []
 for a in authors:
     print "\n" +a + ":" 
-    print_list(sorted(author_bow(a,corpus).items(), key=lambda x: x[1], reverse=True)[:30]) 
+    freqs += [("Author:\t\t",str(a))]
+    print_list(sorted(author_bow(a,corpus).items(), key=lambda x: x[1], reverse=True)[:20]) 
+    freqs+= [(str(x),str(y)) for (x,y) in (sorted(author_bow(a,corpus).items(), key=lambda x: x[1], reverse=True)[:20])]
+save_to_file(freqs,"freqs.txt")
 """
-    
+
+ 
     
     
--- features.py
+++ features.py
@@ -7,38 +7,49 @@ def features(fs, textU):
     """
     text = textU.lower()
     if(fs=="f1"): # ----------------------first feature
-        return (occurs_in_text("legal",text) >0 )
+        return (occurs_in_text("market",text) >0 )
         
     elif(fs=="f2"):# ----------------------second feature
-        return (occurs_in_text("safety",text) >0 )
-        
-    elif (fs=="f3"):# ----------------------third feature
-        return (occurs_in_text("bank",text) >2 )
-        
-    elif(fs=="f4"):# ----------------------second feature
         return (occurs_in_text("internet",text) >0 )
         
+    elif (fs=="f3"):# ----------------------third feature
+        return (occurs_in_text("china",text) >0 )
+        
+    elif(fs=="f4"):# ----------------------second feature
+        return (occurs_in_text("bank",text) >0 )
+        
     elif(fs=="f5"):# ----------------------second feature
-        return (occurs_in_text("mail",text) >1 )
+        return (occurs_in_text("british",text) >0 )
         
     elif(fs=="f6"):# ----------------------second feature
-        return (occurs_in_text("to",text) >5 )
-        
-    elif(fs=="f7"):# ----------------------second feature
-        return (occurs_in_text("china",text) >1 )
-        
-    elif(fs=="f8"):# ----------------------second feature
         return (occurs_in_text("prague",text) >0 )
         
+    elif(fs=="f7"):# ----------------------second feature
+        return (occurs_in_text("wang",text) >0 )
+        
+    elif(fs=="f8"):# ----------------------second feature
+        return (occurs_in_text("beijing",text) >0 )
+        
     elif(fs=="f9"):# ----------------------second feature
-        return (occurs_in_text("british",text) >0 )
-
-    elif(fs=="f10"):# ----------------------second feature
-        return (occurs_in_text("crowns",text) >0 )
-
-    elif(fs=="f11"):# ----------------------second feature
         return (occurs_in_text("million",text) >0 )
 
+    elif(fs=="f10"):# ----------------------second feature
+        return (occurs_in_text("australia",text) >0 )
+
+    elif(fs=="f11"):# ----------------------second feature
+        return (occurs_in_text("to",text) >13 )
+
     elif(fs=="f12"):# ----------------------second feature
-        return (occurs_in_text("court",text) >0 )
+        return (occurs_in_text("was",text) >3 )
+
+    elif(fs=="f13"):# ----------------------second feature
+        return (occurs_in_text("by",text) >3 )
+
+    elif(fs=="f14"):# ----------------------second feature
+        return (occurs_in_text("said",text) >4 )
+
+    elif(fs=="f15"):# ----------------------second feature
+        return (occurs_in_text("had",text) >3 )        
+      
+        
         
